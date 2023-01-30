a3dc9464f0127c181e5c419d195da28d50a4b5a6
Bruno Meilick <b@bnomei.com>
2023-01-30 11:02:33 +0000

    :pencil: non composer config
    related #31
    
    Signed-off-by: bnomei <b@bnomei.com>

--- tests/site/config/config.php
+++ tests/site/config/config.php
@@ -1,5 +1,15 @@
 <?php
 
+//require_once __DIR__ . '/../plugins/kirby3-qrcode/vendor/endroid/qr-code/src/Color/ColorInterface.php';
+//require_once __DIR__ . '/../plugins/kirby3-qrcode/vendor/endroid/qr-code/src/Color/Color.php';
+
 return [
     'debug' => true,
+
+//    'bnomei.qrcode.field' => [
+//        'foregroundColor' => new \Endroid\QrCode\Color\Color(126, 154, 191),
+//        'backgroundColor' => new \Endroid\QrCode\Color\Color(239, 239, 239),
+//        'size' => 128,
+//        'margin' => 0,
+//    ],
 ];
